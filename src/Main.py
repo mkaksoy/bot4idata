2fa242865cde5532a672886019e3e7b9cc677d22
Mehmet Kaan Aksoy <mehmetkaanaksoy13@gmail.com>
2025-04-17 22:28:04 +0300

    v0.2.0

--- src/Main.py
+++ src/Main.py
@@ -1,11 +1,12 @@
 import undetected_chromedriver as uc
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import time
 import os
 import random
 
-def wait_random(min_sec=1.3, max_sec=4.8):
+def wait_random(min_sec=5.3, max_sec=7.8):
     t = random.uniform(min_sec, max_sec)
     print(f"[LOG] Bekleniyor: {t:.2f} saniye...")
     time.sleep(t)
@@ -23,10 +24,29 @@ options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) Apple
 driver = uc.Chrome(options=options, service=service)
 
 try:
-    log("Site: https://www.idata.com.tr/ita/tr")
-    driver.get('https://www.idata.com.tr/ita/tr')
-    wait_random()
+    i = str() # (i)nput
+    while i != "exit":
+        i = input("Enter command (type help for commands):")
+
+        if i == "exit":
+            log("Terminating program...")
+            break
+        elif i == "help":
+            log("Available commands:\nexit: Stop the bot\nhelp: Get help\nstart: Start the bot\npause: Pause the bot\nresume: Resume the bot")
+        elif i == "start":
+            log("Site: https://www.idata.com.tr/ita/tr")
+            driver.get('https://www.idata.com.tr/ita/tr')
+            driver.maximize_window()
+            # Ekran büyütülüyor, Cloudflare JavaScript çalışmasına izin vermediği için şimdilik işlem yok.
+            wait_random()
+            driver.get("https://www.idata.com.tr/ita/tr/p/randevu-islemleri-ita")  # IP anlaşıldıktan sonra artık vize alım sayfasına girilebilir.
+            # İşlemler burada yapılacak
+
+        elif i == ("pause" or "resume"):
+            log("Coming soon...")
+        else:
+            log("Invalid command. Type help for commands.")
 finally:
-    wait_random()
+    log("Program has finished running.")
     log("Closing browser...")
     driver.quit()
